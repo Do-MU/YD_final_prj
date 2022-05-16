a77a673936e9d5df2a18c08fa5717a448927e9ce
been <jybrose@hanmail.net>
2022-05-17 00:02:59 +0900

    0517 계좌목록 불러오기 api 수정, 패키지 정리

--- keumbi/src/main/java/com/keumbi/prj/account/web/AccountController.java
+++ keumbi/src/main/java/com/keumbi/prj/account/web/AccountController.java
@@ -8,26 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.keumbi.prj.common.BankAPI;
+import com.keumbi.prj.bankAPI.BankAPI;
 import com.keumbi.prj.user.vo.UserVO;
 
 @Controller
 public class AccountController {
 
+	// 계좌 view page
 	@RequestMapping("/accountView")
 	public String accountView() {
 		
 		return "account/accountList";
 	}
 	
+	// 계좌목록불러오기
 	@RequestMapping("/accountList")
 	@ResponseBody
-	public String accountList(HttpSession session) {
+	public JsonNode accountList(HttpSession session) {
 		UserVO vo = (UserVO) session.getAttribute("loginUser") ;
 		
 		JsonNode res = BankAPI.getAccountList(vo);
 		
+		//JsonNode -> 변환
+		
 		System.out.println("res : " + res);
-		return "redirect : account/accountList";
+		return res;
 	}
 }
