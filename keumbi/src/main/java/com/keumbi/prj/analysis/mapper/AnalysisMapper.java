b5082e5f2062346a12b0682d8ad11b304bc11463
Hyorim.L <hyolee85@gmail.com>
2022-05-26 18:44:34 +0900

    0526 월별(이번달) 카테고리별 지출 통계 차트 출력 및 당월전월 지출 누적 비교 그래프 출력

--- keumbi/src/main/java/com/keumbi/prj/analysis/mapper/AnalysisMapper.java
+++ keumbi/src/main/java/com/keumbi/prj/analysis/mapper/AnalysisMapper.java
@@ -2,8 +2,10 @@ package com.keumbi.prj.analysis.mapper;
 
 import java.util.List;
 
+import com.keumbi.prj.analysis.vo.AnalysisThisPrevVO;
 import com.keumbi.prj.analysis.vo.AnalysisVO;
 
 public interface AnalysisMapper {
 	List<AnalysisVO> monthlyAnalysis(AnalysisVO vo); 		// 월별 지출 통계 데이터 -> 파이차트
+	List<AnalysisThisPrevVO> thisPreAnalysis(AnalysisThisPrevVO vo);		// 당월전월 지출금액 통계 데이터 -> 영역차트
 }
---
+++ keumbi/src/main/java/com/keumbi/prj/analysis/vo/AnalysisThisPrevVO.java
@@ -0,0 +1,14 @@
+package com.keumbi.prj.analysis.vo;
+
+import lombok.Data;
+
+@Data
+public class AnalysisThisPrevVO {
+
+	private String dt;
+	private int amt1;
+	private int amt2;
+	private String user_id;
+	private String thisMonth;
+	private String lastMonth;
+}
