422ba1e57dab111b2afb6afdc81bf15fcc7c2d7c
Hyorim.L <hyolee85@gmail.com>
2022-05-27 18:18:08 +0900

    0527 지출분석 -> 일별/월별/연도별 지출통계차트 출력 구현(날짜구하는함수 재검토 예정)

--- keumbi/src/main/java/com/keumbi/prj/analysis/serviceImpl/AnalysisServiceImpl.java
+++ keumbi/src/main/java/com/keumbi/prj/analysis/serviceImpl/AnalysisServiceImpl.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.keumbi.prj.analysis.mapper.AnalysisMapper;
 import com.keumbi.prj.analysis.service.AnalysisService;
+import com.keumbi.prj.analysis.vo.AnalysisDailyVO;
+import com.keumbi.prj.analysis.vo.AnalysisMonthlyVO;
 import com.keumbi.prj.analysis.vo.AnalysisThisPrevVO;
 import com.keumbi.prj.analysis.vo.AnalysisVO;
+import com.keumbi.prj.analysis.vo.AnalysisYearlyVO;
 @Service
 public class AnalysisServiceImpl implements AnalysisService {
 
@@ -22,6 +25,21 @@ public class AnalysisServiceImpl implements AnalysisService {
 	@Override
 	public List<AnalysisThisPrevVO> thisPreAnalysis(AnalysisThisPrevVO vo) {
 		return m.thisPreAnalysis(vo);
+	}
+
+	@Override
+	public List<AnalysisMonthlyVO> columnChartMonthly(AnalysisMonthlyVO vo) {
+		return m.columnChartMonthly(vo);
+	}
+
+	@Override
+	public List<AnalysisDailyVO> columnChartDaily(AnalysisDailyVO vo) {
+		return m.columnChartDaily(vo);
+	}
+
+	@Override
+	public List<AnalysisYearlyVO> columnChartYearly(AnalysisYearlyVO vo) {
+		return m.columnChartYearly(vo);
 	}
 
 }
---
+++ keumbi/src/main/java/com/keumbi/prj/analysis/vo/AnalysisMonthlyVO.java
@@ -0,0 +1,14 @@
+package com.keumbi.prj.analysis.vo;
+
+import lombok.Data;
+
+@Data
+public class AnalysisMonthlyVO {
+	private String user_id;
+	private String month;
+	private int amt;
+	private String firstMonth;
+	private String lastMonth;
+	
+
+}
